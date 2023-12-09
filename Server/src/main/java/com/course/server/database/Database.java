9b92d720dee4ea18643432f0feb1aa848f33c299
Dmitry Osobov <waffencode@gmail.com>
2023-12-09 22:38:33 +0300

    getUser implementation

--- Server/src/main/java/com/course/server/database/Database.java
+++ Server/src/main/java/com/course/server/database/Database.java
@@ -2,14 +2,20 @@ package com.course.server.database;
 
 import com.course.server.domain.InventoryObject;
 import com.course.server.domain.InventoryObjectsList;
+import com.course.server.domain.Role;
 import com.course.server.domain.User;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class Database
 {
+    private static final String DB_URL = "jdbc:mysql://localhost/inventory";
+    private static final String USER = "root";
+    private static final String PASS = "+WHLm0WsP^j6hr7mfD58";
+
     public void createUser(User user)
     {
 
@@ -17,7 +23,27 @@ public class Database
 
     public User getUser(UUID id)
     {
-        return new User();
+        User user = new User();
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM users WHERE id = ?;"))
+        {
+            pstmt.setString(1, id.toString());
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next())
+            {
+                user.setId(UUID.fromString(rs.getString("id")));
+                user.setLogin(rs.getString("login"));
+                user.setPasswordHash(rs.getString("password_hash"));
+                user.setRole(Role.valueOf(rs.getString("role")));
+                user.setRegistrationDate(Date.valueOf(rs.getString("registration_date")));
+            }
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     public List<User> getAllUsers(UUID id)
--- Server/src/main/java/com/course/server/domain/User.java
+++ Server/src/main/java/com/course/server/domain/User.java
@@ -10,4 +10,54 @@ public class User
     private String passwordHash;
     private Role role;
     private Date registrationDate;
+
+    public UUID getId()
+    {
+        return id;
+    }
+
+    public void setId(UUID id)
+    {
+        this.id = id;
+    }
+
+    public String getLogin()
+    {
+        return login;
+    }
+
+    public void setLogin(String login)
+    {
+        this.login = login;
+    }
+
+    public String getPasswordHash()
+    {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash)
+    {
+        this.passwordHash = passwordHash;
+    }
+
+    public Role getRole()
+    {
+        return role;
+    }
+
+    public void setRole(Role role)
+    {
+        this.role = role;
+    }
+
+    public Date getRegistrationDate()
+    {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate)
+    {
+        this.registrationDate = registrationDate;
+    }
 }
