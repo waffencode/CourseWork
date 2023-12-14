c610c14beb6d2c7f3f0460681b2ff5db340a09be
Dmitry Osobov <waffencode@gmail.com>
2023-12-14 13:34:52 +0300

    Add view for merging lists

--- Client/src/main/java/com/course/client/controllers/inventory/MainListController.java
+++ Client/src/main/java/com/course/client/controllers/inventory/MainListController.java
@@ -89,6 +89,12 @@ public class MainListController extends SceneController
     }
 
     @FXML
+    private void onMergeButtonClicked()
+    {
+        goToSceneWithResource("Inventory/MergeListView.fxml");
+    }
+
+    @FXML
     private void onDeleteButtonClicked()
     {
         if (listsView.getSelectionModel().getSelectedItem() == null)
---
+++ Client/src/main/java/com/course/client/controllers/inventory/MergeListController.java
@@ -0,0 +1,89 @@
+package com.course.client.controllers.inventory;
+
+import com.course.client.domain.InventoryObject;
+import com.course.client.domain.InventoryObjectsList;
+import com.course.client.service.ModelContext;
+import com.course.client.service.UiContext;
+import com.course.client.ui.NotificationDialog;
+import com.course.client.ui.SceneController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+import java.util.UUID;
+
+public class MergeListController extends SceneController
+{
+    @FXML
+    private ChoiceBox<InventoryObjectsList> selectTargetList, selectAuxList;
+
+    @FXML
+    private TextField newListName;
+
+    @Override
+    public void setContext(ModelContext modelContext, UiContext uiContext)
+    {
+        this.modelContext = modelContext;
+        this.uiContext = uiContext;
+        updateList();
+    }
+
+    private void updateList()
+    {
+        UUID currentUserId = modelContext.getCurrentUser().getId();
+        List<InventoryObjectsList> list = modelContext.getRequestHandler().getAllLists(currentUserId);
+        ObservableList<InventoryObjectsList> inventoryObjectsLists = FXCollections.observableArrayList(list);
+        selectTargetList.setItems(inventoryObjectsLists);
+        selectAuxList.setItems(inventoryObjectsLists);
+    }
+
+    @FXML
+    private void onBackButtonClicked()
+    {
+        goToSceneWithResource("Inventory/MainListsView.fxml");
+    }
+
+    @FXML
+    private void onApplyButtonClicked()
+    {
+        InventoryObjectsList target = selectTargetList.getSelectionModel().getSelectedItem();
+        InventoryObjectsList aux = selectAuxList.getSelectionModel().getSelectedItem();
+
+        if (target == null || aux == null || newListName.getText().isBlank())
+        {
+            NotificationDialog.showWarningDialog("Выберите списки для объединения и введите название нового списка!");
+        }
+
+        if (target.getId() == aux.getId())
+        {
+            NotificationDialog.showWarningDialog("Невозможно объединить одинаковые списки!");
+        }
+
+        InventoryObjectsList mergedList = new InventoryObjectsList();
+        mergedList.setName(newListName.getText());
+        mergedList.setCreatedById(modelContext.getCurrentUser().getId());
+        modelContext.getRequestHandler().createList(mergedList, modelContext.getCurrentUser().getId());
+
+        List<InventoryObject> targetListObjects = modelContext.getRequestHandler().getAllObjectsFromList(target.getId(), modelContext.getCurrentUser().getId());
+        changeListIdForObjects(targetListObjects, mergedList.getId());
+        List<InventoryObject> auxListObjects = modelContext.getRequestHandler().getAllObjectsFromList(aux.getId(), modelContext.getCurrentUser().getId());
+        changeListIdForObjects(auxListObjects, mergedList.getId());
+
+        modelContext.getRequestHandler().deleteList(target.getId(), modelContext.getCurrentUser().getId());
+        modelContext.getRequestHandler().deleteList(aux.getId(), modelContext.getCurrentUser().getId());
+
+        goToSceneWithResource("Inventory/MainListsView.fxml");
+    }
+
+    private void changeListIdForObjects(List<InventoryObject> objects, UUID listID)
+    {
+        for (InventoryObject object: objects)
+        {
+            object.setListId(listID);
+            modelContext.getRequestHandler().updateObject(object, modelContext.getCurrentUser().getId());
+        }
+    }
+}
